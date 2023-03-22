3292f0f35d9b25886f4a01c72ff765f9533d8216
unknown <Administrator@PC4-LAB2.labs.org>
2023-03-22 20:19:50 +0500

    Hello world

---
+++ tab.js
@@ -0,0 +1,22 @@
+
+   
+ 
+    $(document).ready(function(){
+        $("#btn").click(function(){
+          $(".red").show();
+
+       
+  
+        });
+      });
+      $(document).ready(function(){
+        $("#cancel").click(function(){
+          $(".red").hide();
+
+       
+  
+        });
+      });
+  
+
+      
